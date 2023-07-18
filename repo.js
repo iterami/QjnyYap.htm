25bca4bc605fb28792f8e40c001a7fbde7860b66
Jan Hořava <czechinthemail@gmail.com>
2023-07-18 14:23:35 -0700

    combined js into repo.js

--- repo.js
+++ repo.js
@@ -1,5 +1,28 @@
 'use strict';
 
+function load_data(){
+    let loop_counter = Math.max(
+      core_storage_data['rings'] - 1,
+      0
+    );
+    do{
+        let inner_counter = loop_counter;
+        do{
+            entity_create({
+              'properties': {
+                'color': '#' + core_random_hex(),
+                'height': 10,
+                'layer': loop_counter + 1,
+                'rotation': inner_counter,
+                'width': 10,
+              },
+            });
+        }while(inner_counter--);
+    }while(loop_counter--);
+
+    rotation_rate = .005;
+}
+
 function repo_drawlogic(){
     entity_group_modify({
       'groups': [
